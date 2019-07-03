6eb40e68573693afd26a53d4fb31affa9b4b8dce
iFaujack <fauzi.nurfadillahh@gmail.com>
2019-07-03 17:15:36 +0700

    Update Rabu

--- exercise-9.js
+++ exercise-9.js
@@ -3,37 +3,19 @@ function checkAB(num) {
     num = num.split("");
     var print = false;
     for (var i=0; i<num.length; i++){
-        if (num[i]==="a"||num[i]==="b"){
-            // console.log(num[i],i)
-            for(var j=i+i; j<num.length; j++){
-                
-                if (num[i] === "a"){
-                    // console.log("Check A",num[j],j)
-                    if (num[j] === "b"){
-                        if (j-i >= 3){
-                            // console.log("Berhasil check A di",i,j)
-                            print = true;
-                        } else {
-                            j = num.length;
-                        }
-                    } 
-                } else if (num[i] === "b"){
-                    // console.log("Check B",num[j],j)
-                    if (num[j] === "a"){
-                        if (j-i >= 3){
-                        // console.log("Berhasil check B di",i,j)
-                        print = true;
-                        } else {
-                            j = num.length;
-                        }
-                    } 
-                }
-            }
-        }
+       if (num[i] === "a"){
+           if (num[i+4] === "b"){
+               print = true;
+               break;
+           }
+       } else if (num[i] === "b"){
+           if (num[i+4] === "a"){
+               print = true;
+               break
+           }
+       }
     }
-
     return print;
-
   }
   
   // TEST CASES
